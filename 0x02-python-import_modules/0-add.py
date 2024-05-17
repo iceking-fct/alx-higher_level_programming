1951c72fbda65ced31ba55612b26077284f44665
M-kendrick <miracleamajama14@gmail.com>
2024-05-16 23:47:58 -0700

    0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+# Let's import the add_0 function module
+
+from add_0 import add
+
+# initial the variable arguments
+a = 1
+b = 2
+
+result = add(a, b)
+
+print(f"{a} + {b} = {result}")
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,4 @@
+#This is the add_0 function module to be imported
+
+def add (a, b):
+    return a+b
