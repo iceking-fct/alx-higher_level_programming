7a0f804e05fe38938452853e196322b5b67d0f1e
iceking-fct <miracleamajama14@gmail.com>
2024-09-21 18:05:28 +0100

    1-last_digit.py updated

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 ld = number % 10 if number > 0 else (abs(number) % 10) * -1
 
-
 # function to create response message
+
+
 def getmsg(num):
     if num > 5:
         return "greater than 5"
@@ -13,4 +15,6 @@ def getmsg(num):
     else:
         return "0"
 
+
+# Corrected print statement
 print(f"Last digit of {number} is {ld} and is {getmsg(ld)}")
